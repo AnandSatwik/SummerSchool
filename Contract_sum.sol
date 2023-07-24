ef2ce95683323ef0babcf3a411776c0c38570d47
AnandSatwik <114983307+AnandSatwik@users.noreply.github.com>
2023-07-25 00:15:16 +0530

    Update Contract_sum.sol

--- Contract_sum.sol
+++ Contract_sum.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8;
 
 contract TicketingContract {
 
@@ -9,53 +9,49 @@ contract TicketingContract {
         bool isValid;
     }
 
-  
+    event TicketTransfer(uint indexed ticketid, address indexed from, address indexed to);
+
     mapping(uint => Ticket) public tickets;
-    mapping(address=>bool) public veri_owners;
-    
+    mapping(address => bool) public veri_owners;
 
-    
-    event TicketTransfer(uint indexed ticketId, address indexed from, address indexed to);
+    constructor(address[] memory owners) {
+        for (uint i = 0; i < owners.length; i++) {
+            veri_owners[owners[i]] = true;
+        }
+    }
 
-    modifier onlyverified_owner(){
-        require(veri_owners[msg.sender],"You are not a verified owner");
+    modifier onlyverified_owner() {
+        require(veri_owners[msg.sender], "You are not a verified owner");
         _;
     }
+
     modifier onlyTicketOwner(uint ticketId) {
         require(tickets[ticketId].owner == msg.sender, "Only ticket owner can perform this action");
         _;
     }
-    
-    constructor(address[] memory owners){
-        for(uint i=0;i<owners.length;i++){
-            veri_owners[owners[i]]=true;
-        }
 
-    }
-
-    
-    function createTicket(uint ticketId, uint eventId) external onlyverified_owner {
+    function createTickets(uint ticketId, uint eventId) external onlyverified_owner {
         require(tickets[ticketId].owner == address(0), "Ticket Id already exists");
-        require(tickets[ticketId].isValid == false, "Ticket Id already exists") ;
+        require(tickets[ticketId].isValid == false, "Ticket Id already exists");
         tickets[ticketId] = Ticket(eventId, address(0), true);
     }
 
-    
-    function purchaseTicket(uint ticketId) external payable {
-        require(tickets[ticketId].isValid, "Invalid ticket ID");
-        require(tickets[ticketId].owner == address(0), "Ticket is already owned");
+    function purchaseTicket(uint eventId) external payable {
+        uint ticketId = uint(keccak256(abi.encodePacked(eventId, msg.sender)));
+        require(tickets[ticketId].owner == address(0), "Ticket Id already exists");
+        require(tickets[ticketId].isValid == true, "Ticket not available for purchase");
+        tickets[ticketId].eventId = eventId;
         tickets[ticketId].owner = msg.sender;
     }
 
-    
     function transferTicket(uint ticketId, address to) external onlyTicketOwner(ticketId) {
-        require(tickets[ticketId].isValid, "Invalid ticket ID");
+        require(tickets[ticketId].isValid == true, "Invalid ticket Id");
         require(to != address(0), "Invalid recipient address");
         tickets[ticketId].owner = to;
         emit TicketTransfer(ticketId, msg.sender, to);
     }
-    
-    function invalidateTicket(uint ticketId) external onlyTicketOwner(ticketId) {
+
+    function invalidateTickets(uint ticketId) external onlyTicketOwner(ticketId) {
         tickets[ticketId].isValid = false;
     }
 }
